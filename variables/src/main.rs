77db9240b95fd979fd0962b9c61748657701ff10
Infant Frontender <newbornfrontender@gmail.com>
2019-07-24 11:05:46 +0300

    feat(variables): add const sample

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is {}", x);
+    const MAX_POINTS: u32 = 100_000;
+    let first_name = String::from("Jack");
 
-    x = 6;
-    println!("The value of x is {}", x);
+    println!("Max points is {}", MAX_POINTS);
+    println!("His name is {}", first_name);
 }
